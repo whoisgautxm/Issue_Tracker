713214a48bacc17d89c1b3771d28c7c94649769e
Gautam Kumar <129857715+whoisgautxm@users.noreply.github.com>
2023-12-28 15:34:35 +0530

    style the active link in the navbar

--- issue_tracker/app/Navbar.tsx
+++ issue_tracker/app/Navbar.tsx
@@ -1,27 +1,41 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 import { AiFillBug } from "react-icons/ai";
-
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames'
 
 const Navbar = () => {
+    const currentPath = usePathname();
 
     const links = [
-        {labels: 'Dashboard', href: '/'},
-        {labels: 'Issues', href: '/'},
-    ]
-  return (
-    <div>
-      <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/"><AiFillBug/></Link>
-        <ul className='flex space-x-6'>
-            {links.map(link => 
-            <Link key = {link.href}
-            className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.labels}</Link>)}
-        
-        </ul>
-      </nav>
-    </div>
-  )
+        { label: 'Dashboard', href: '/' },
+        { label: 'Issues', href: '/issues' },
+    ];
+
+    return (
+        <div>
+            <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+                <Link href="/"><AiFillBug /></Link>
+                <ul className='flex space-x-6'>
+                    {links.map(link => (
+                        <li key={link.href}>
+                            <Link
+                                href={link.href}
+                                className={classnames({
+                                    'text-zinc-900': currentPath === link.href,
+                                    'text-zinc-500': currentPath !== link.href,
+                                    'hover: text-zinc-800 transition-colors' : true
+                                })}
+                            >
+                                {link.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
